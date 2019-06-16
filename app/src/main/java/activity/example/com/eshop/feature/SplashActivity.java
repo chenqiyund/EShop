7a8e1b8b10a7e00eb73f8e7bcd40a08ddd426e7e
chenqiyun <970785139@qq.com>
2019-06-16 11:48:51 +0800

    前置资源导入.

---
+++ app/src/main/java/activity/example/com/eshop/feature/SplashActivity.java
@@ -0,0 +1,19 @@
+package activity.example.com.eshop.feature;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import activity.example.com.eshop.R;
+
+/**
+ * Created by Administrator on 2019/6/15.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
