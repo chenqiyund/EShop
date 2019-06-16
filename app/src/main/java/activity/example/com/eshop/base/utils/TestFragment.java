858a8b8be427d9b3a2e4888af459a18c8b6ae82a
chenqiyun <970785139@qq.com>
2019-06-16 15:45:16 +0800

    主页面：Fragment的切换与处理 .

---
+++ app/src/main/java/activity/example/com/eshop/base/utils/TestFragment.java
@@ -0,0 +1,51 @@
+package activity.example.com.eshop.base.utils;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import activity.example.com.eshop.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+
+public class TestFragment extends Fragment {
+    private static final String FRAGMENT_TEXT = "fragment_text";
+
+    @BindView(R.id.text)
+    TextView mTextView;
+
+    // 对外提供一个创建方法：传递数据
+    public static TestFragment newInstance(String text){
+        TestFragment testFragment = new TestFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString(FRAGMENT_TEXT,text);
+        testFragment.setArguments(bundle);
+
+        return testFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_text,container,false);
+
+        ButterKnife.bind(this,view);
+        mTextView.setText(getArgumentText());
+        return view;
+    }
+
+    public String getArgumentText(){
+        return getArguments().getString(FRAGMENT_TEXT);
+    }
+}
