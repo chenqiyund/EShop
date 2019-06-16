b9c04e559056053968ea451e9fdcb285520a562e
chenqiyun <970785139@qq.com>
2019-06-16 12:06:46 +0800

    内存泄露的加入.

--- app/src/main/java/activity/example/com/eshop/feature/SplashActivity.java
+++ app/src/main/java/activity/example/com/eshop/feature/SplashActivity.java
@@ -1,10 +1,12 @@
 package activity.example.com.eshop.feature;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import activity.example.com.eshop.R;
+import activity.example.com.eshop.base.utils.LogUtils;
 
 /**
  * Created by Administrator on 2019/6/15.
