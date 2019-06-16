f54993fa4536f86e98db0016d3bff400b1b79d56
chenqiyun <970785139@qq.com>
2019-06-16 12:44:30 +0800

    Splash: 页面的搭建及实现.

---
+++ app/src/main/java/activity/example/com/eshop/feature/EShopMainActivity.java
@@ -0,0 +1,19 @@
+package activity.example.com.eshop.feature;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import activity.example.com.eshop.R;
+
+/**
+ * Created by Administrator on 2019/6/16.
+ */
+
+public class EShopMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eshop_main);
+    }
+}
