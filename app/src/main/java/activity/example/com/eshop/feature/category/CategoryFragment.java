154db44f5e07692c08b88e54a6292bb25474fbff
chenqiyun <970785139@qq.com>
2019-06-17 14:51:13 +0800

    Toolbar的处理 .

--- app/src/main/java/activity/example/com/eshop/feature/category/CategoryFragment.java
+++ app/src/main/java/activity/example/com/eshop/feature/category/CategoryFragment.java
@@ -7,10 +7,15 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import activity.example.com.eshop.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,18 +46,6 @@ public class CategoryFragment extends Fragment {
         return view;
     }
 
-    /**
-     * @Nullable
-     @Override
-     public View onCreateView(Layout inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-     View view = inflater.inflate(R.layout.fragment_category, container, false);
-     ButterKnife.bind(this, view);
-     return view;
-     }
-     *
-     */
-
-
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -69,8 +62,45 @@ public class CategoryFragment extends Fragment {
         // 处理actionbar不展示默认的标题
         activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
         mToolbarTitle.setText(R.string.category_title);
-
+        
+        initToolbar();
         // ListView的展示
 
+    }
+
+    private void initToolbar() {
+        // Fragment显示选项菜单
+        setHasOptionsMenu(true);
+        // 处理toolbar
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(mToolbar);
+        // 处理actionbar不展示默认的标题
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // 设置左上方的返回箭头
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbarTitle.setText(R.string.category_title);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_category,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        // 返回箭头
+        if (itemId == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+
+        if (itemId == R.id.menu_search) {
+
+            // TODO: 2017/2/24 后期会跳转到搜素页面上
+            Toast.makeText(getContext(), "点击了搜索", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
