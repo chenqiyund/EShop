9bf55661783a9cc2d572c65c87a931ce2465b29d
chenqiyun <970785139@qq.com>
2019-06-17 20:46:48 +0800

    布局搭建：Toolbar和刷新控件 .

--- app/src/main/java/activity/example/com/eshop/feature/home/HomeFragment.java
+++ app/src/main/java/activity/example/com/eshop/feature/home/HomeFragment.java
@@ -1,5 +1,11 @@
 package activity.example.com.eshop.feature.home;
 
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +14,9 @@ import activity.example.com.eshop.base.BaseFragment;
 import activity.example.com.eshop.base.widgets.banner.BannerAdapter;
 import activity.example.com.eshop.base.widgets.banner.BannerLayout;
 import butterknife.BindView;
+import in.srain.cube.views.ptr.PtrClassicDefaultHeader;
+import in.srain.cube.views.ptr.PtrDefaultHandler2;
+import in.srain.cube.views.ptr.PtrFrameLayout;
 
 /**
  * Created by Administrator on 2019/6/17.
@@ -15,10 +24,16 @@ import butterknife.BindView;
 
 public class HomeFragment extends BaseFragment {
 
-    @BindView(R.id.layout_banner)
-    BannerLayout mBannerLayout;
+    @BindView(R.id.standard_toolbar_title)
+    TextView mToolbarTitle;
+    @BindView(R.id.standard_toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.list_home_goods)
+    ListView mListHomeGoods;
+    @BindView(R.id.standard_refresh_layout)
+    PtrFrameLayout mRefreshLayout;
 
-    public static HomeFragment newInstance(){
+    public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
@@ -29,23 +44,58 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        initToolbar();
+        initPtr();
+    }
 
-        // 给BannerLayout设置适配器
-        BannerAdapter<Object> bannerAdapter= new BannerAdapter<Object>() {
+    // 刷新的初始化
+    private void initPtr() {
+        // 设置刷新显示的头布局、刷新事件等
+        mRefreshLayout.disableWhenHorizontalMove(true);
+
+        // 设置刷新显示的头布局：默认的
+        PtrClassicDefaultHeader header = new PtrClassicDefaultHeader(getContext());
+        mRefreshLayout.setHeaderView(header);
+        mRefreshLayout.addPtrUIHandler(header);
+
+        // 设置刷新处理的Handler
+        mRefreshLayout.setPtrHandler(mPtrHandler);
+
+
+        // 一进来我们就去刷新：自动刷新的方法，延时任务然后自动刷新
+        mRefreshLayout.postDelayed(new Runnable() {
             @Override
-            protected void bind(ViewHolder holder, Object data) {
-                holder.mImageView.setImageResource(R.drawable.image_holder_banner);
+            public void run() {
+                // 自动刷新
+                mRefreshLayout.autoRefresh();
             }
-        };
-        mBannerLayout.setAdapter(bannerAdapter);
+        },50);
+    }
 
-        // 添加模拟的数据
-        List<Object> data = new ArrayList<>();
-        data.add(new Object());
-        data.add(new Object());
-        data.add(new Object());
-        data.add(new Object());
-        data.add(new Object());
-        bannerAdapter.reset(data);
+    private PtrDefaultHandler2 mPtrHandler = new PtrDefaultHandler2() {
+
+        // 加载时触发
+        @Override
+        public void onLoadMoreBegin(PtrFrameLayout frame) {
+
+        }
+
+        // 刷新的时候触发
+        @Override
+        public void onRefreshBegin(PtrFrameLayout frame) {
+
+        }
+    };
+
+    // 初始化Toolbar的操作
+    private void initToolbar() {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        // 设置Toolbar作为ActionBar
+        activity.setSupportActionBar(mToolbar);
+        ActionBar actionBar = activity.getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+        mToolbarTitle.setText(R.string.home_title);
     }
 }
