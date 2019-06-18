0329015c40c5415a710e77ef0eed4686e28403b2
chenqiyun <970785139@qq.com>
2019-06-18 12:35:43 +0800

    基类中添加请求方法 .

--- app/src/main/java/activity/example/com/eshop/base/BaseActivity.java
+++ app/src/main/java/activity/example/com/eshop/base/BaseActivity.java
@@ -3,8 +3,13 @@ package activity.example.com.eshop.base;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 
+import activity.example.com.eshop.network.EShopClient;
+import activity.example.com.eshop.network.core.ApiInterface;
+import activity.example.com.eshop.network.core.ResponseEntity;
+import activity.example.com.eshop.network.core.UICallback;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.Call;
 
 /**
  * Created by Administrator on 2019/6/17.
@@ -27,10 +32,27 @@ public abstract class BaseActivity extends TransitionActivity{
 
     // 让子类来告诉我们布局
     @LayoutRes protected abstract int getContentViewLayout();
+    // 基类中提供一个请求的方法
+    protected Call enqueue(final ApiInterface apiInterface){
+
+        UICallback uiCallback = new UICallback() {
+            @Override
+            public void onBusinessResponse(boolean isSucces, ResponseEntity responseEntity) {
+                // 处理数据：提供给使用者处理
+                BaseActivity.this.onBusinessResponse(apiInterface.getPath(),isSucces,responseEntity);
+            }
+        };
+
+        return EShopClient.getInstance().enqueue(apiInterface,uiCallback,getClass().getSimpleName());
+    }
+
+    protected abstract void onBusinessResponse(String path,boolean isSucces, ResponseEntity responseEntity);
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // 取消请求
+        EShopClient.getInstance().CancelByTag(getClass().getSimpleName());
         mUnbinder.unbind();
         mUnbinder = null;
     }
--- app/src/main/java/activity/example/com/eshop/base/BaseFragment.java
+++ app/src/main/java/activity/example/com/eshop/base/BaseFragment.java
@@ -8,8 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import activity.example.com.eshop.network.EShopClient;
+import activity.example.com.eshop.network.core.ApiInterface;
+import activity.example.com.eshop.network.core.ResponseEntity;
+import activity.example.com.eshop.network.core.UICallback;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import okhttp3.Call;
 
 /**
  * Created by Administrator on 2019/6/17.
@@ -32,10 +37,26 @@ public abstract class BaseFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         initView();
     }
+    // 提供一个请求方法
+    protected Call enqueue(final ApiInterface apiInterface){
+
+        UICallback uiCallback = new UICallback() {
+            @Override
+            public void onBusinessResponse(boolean isSucces, ResponseEntity responseEntity) {
+                // 通过一个抽象方法将数据传递出去
+                BaseFragment.this.onBusinessResponse(apiInterface.getPath(),isSucces,responseEntity);
+            }
+        };
+
+        return EShopClient.getInstance().enqueue(apiInterface,uiCallback,getClass().getSimpleName());
+    }
+
+    protected abstract void onBusinessResponse(String path,boolean isSucces, ResponseEntity responseEntity);
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        EShopClient.getInstance().CancelByTag(getClass().getSimpleName());
         mUnbinder.unbind();
         mUnbinder = null;
     }
